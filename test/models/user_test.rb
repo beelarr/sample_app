4334c9b12ae3d5ac05390776349cf56867b8a21a
beelarr <bryonl@me.com>
2017-10-22 14:39:20 -0500

    Make a basic User model including secure passwords and name/email validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Bryon Larrance", email: "bryonl@me.com")
+    @user = User.new(name: "Bryon Larrance", email: "bryonl@me.com",
+                      password: "foobar", password_confirmation: "foobar")
   end
   test "should be vaild" do
     assert @user.valid?
@@ -23,4 +24,30 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@esample.com"
     assert_not @user.valid?
   end
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_food.ord user.name@example. foo@var baz.com foo@bar+baz.com]
+    invalid_addresses.each do |i|
+      assert @user.valid?, "#{i.inspect} should be valid"
+    end
+  end
+  test "eamil addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  test "email addresses should be saved as downcase" do
+    mixed_case_email = "FoO@Baar.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  test "password should be present not blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+  test "password should have a minimum lenght" do
+    @user.password = @user.password_confirmation = "a" * 5
+  end
 end
+
