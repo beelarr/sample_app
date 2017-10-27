87cd255e0f1c4a65fa79d30eca67b2b5f950e56c
beelarr <bryonl@me.com>
2017-10-27 10:34:05 -0500

    Updated puma.rb file

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,15 @@
-test "valid signup information" do
-  get signup_path
-  assert_difference 'User.count', 1 do
-    post users_path, params: { user: { name:  "Example User",
-                                       email: "user@example.com",
-                                       password:              "password",
-                                       password_confirmation: "password" } }
-  end
-  follow_redirect!
-  assert_template 'users/show'
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
