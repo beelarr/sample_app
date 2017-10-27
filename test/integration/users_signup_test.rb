cb202776aa1935a4b1e62c2e13b27bb9c6dec6fc
beelarr <bryonl@me.com>
2017-10-27 12:04:21 -0500

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert flash.present?
+    assert is_logged_in?
   end
 end
