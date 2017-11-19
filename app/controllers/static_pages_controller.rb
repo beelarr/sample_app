1fbae95a8cd8857540fa354b7cb839f92f54810c
beelarr <bryonl@me.com>
2017-11-18 19:14:52 -0600

    13.3.4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
-
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
@@ -9,6 +11,8 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
   def contact
   end
 end
+
